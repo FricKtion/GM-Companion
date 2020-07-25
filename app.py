d8a5b65b227dc06f478067acb4c1f5f26d632dae
Cody Frick <frick_95@yahoo.com>
2020-07-25 18:48:32 -0400

    Added a bootstrap template and some fill-in logic for the flask part of the app.

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from ArchivesScraper import ArchivesScraper
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route("/Monsters")
+def Monsters():
+    scraper = ArchivesScraper()
+    monsters = scraper.MonsterList()
+    
+    result = ""
+    for monster in sorted(monsters, key=monsters.get):
+       result += monster + ": " + str(monsters[monster])
+       result += "<br />"
+
+    return result
+
+@app.route("/NPCs")
+def NPCs():
+    scraper = ArchivesScraper()
+    npcs = scraper.NpcList()
+    
+    result = ""
+    for npc in sorted(npcs, key=npcs.get):
+        result += npc + ": " + str(npcs[npc])
+        result += "<br />"
+
+    return result
+
+if __name__ == "__main__":
+    app.run()
